6b18b035275708f922a86b19660736adc8eafeaf
Luan A. <barkon32@tuta.io>
2023-12-02 16:04:40 -0300

    new butto implementation

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Form from './components/Form';
 import Header from './components/Header';
 import styles from './App.module.css'
 import Button from './components/Button';
+import { PlusCircle } from '@phosphor-icons/react';
 
 function App() {
 
@@ -13,7 +14,10 @@ function App() {
         <div className={styles.content}>
           <div className={styles.taskCreationContainer}>
             <Form />
-            <Button />
+            <Button>
+              Criar
+              <PlusCircle size={16} color='#f2f2f2' weight='bold'/>
+            </Button>
           </div>
         </div>
       </main>
