58f781236dddd0cd2b1b2178dcebe60e1ef18e1c
Luan A. <barkon32@tuta.io>
2023-12-02 16:06:29 -0300

    removed Form component

--- src/components/Form.tsx
+++
@@ -1,7 +0,0 @@
-import styles from './Form.module.css'
-
-export default function Form() {
-  return (
-    <input className={styles.textbox} type="text" placeholder='Adicione uma nova tarefa' />
-  )
-}
