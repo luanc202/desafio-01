1ccea88fa6dde5c89df12fe6399dbea8cf80a9ff
Luan A. <barkon32@tuta.io>
2023-12-02 14:26:12 -0300

    removed margin and fixed header position

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import './App.css';
+import Form from './components/Form';
 import Header from './components/Header';
 
 function App() {
@@ -8,6 +8,8 @@ function App() {
     <>
       <main>
         <Header/>
+
+        <Form/>
       </main>
     </>
   )
