099bbc61c7cca7332a73cea34b748a2ca044834d
Luan A. <barkon32@tuta.io>
2023-12-02 15:51:18 -0300

    changed button into a generic component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,9 @@
 import styles from './Button.module.css'
 
-export default function Button() {
+type Props = React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
+
+export default function Button({ children, ...rest }: Props) {
   return (
-    <button className={styles.button}>Criar</button>
+    <button className={styles.button} {...rest}>{children}</button>
   )
 }
